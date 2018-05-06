e2603bf98b15d9aa883e3eef43c02108e4c00ca6
Junard Lebajan <junard.lebajan@icloud.com>
2018-05-06 13:18:50 +0800

    Continued String Analysis Exercise.

--- iGoat-Swift/iGoat-Swift/Source/Exercises/Reverse Engineering/StringAnalysisExerciseVC.swift
+++ iGoat-Swift/iGoat-Swift/Source/Exercises/Reverse Engineering/StringAnalysisExerciseVC.swift
@@ -1,18 +1,12 @@
 import UIKit
 
-/*
-enum SAConstants {
-    static let stringAnalysisExercise = "secret plaintext riddle answer: To prove it was not chicken";
-}
-*/
- 
 class StringAnalysisExerciseVC: UIViewController {
 
     @IBOutlet weak var answerTextField: UITextField!
-    
+        
     @IBAction func submit() {
-        guard answerTextField.text?.isEmpty ?? false
-            else {
+        
+        if answerTextField.text!.isEmpty {
             UIAlertController.showAlertWith(title: "iGoat", message: "Empty entry!!")
             return
         }
@@ -34,16 +28,21 @@ class StringAnalysisExerciseVC: UIViewController {
     }
 }
 
+
+enum SAConstants {
+    static let stringAnalysisExercise = "to prove it wasn’t chicken"
+}
+
 extension StringAnalysisExerciseVC {
     func retrieveStringTableEntry() -> String {
-        return "" //SAConstants.stringAnalysisExercise
+        return SAConstants.stringAnalysisExercise
     }
-    
+
     func isValidResponse(proposedText: String) -> Bool {
         let stringTableEntry = retrieveStringTableEntry()
         return stringTableEntry.contains(proposedText)
     }
-    
+
     func isPlaintextStringTableEntry() -> Bool {
         return false
     }
@@ -54,8 +53,8 @@ extension StringAnalysisExerciseVC {
 //Solution on cryptic encoding.
 /*
 enum SAConstants {
-    static let stringAnalysisExercise = "EhEKNQoVVBsuCwUYDGcfDRUAKRsEDB1nDg8HHiIdW1Q9KE8RBgYxCkEdHWcYAAcHYBtBFwEuDAoRBw=="
- }
+    static let stringAnalysisExercise = "HShPEQYGMQpBHR1nGAAHB6Xv+ABJJAcIFwIiAQ=="
+}
  
 extension StringAnalysisExerciseVC {
     
@@ -66,7 +65,7 @@ extension StringAnalysisExerciseVC {
         var encrypted = [UInt8]()
         // encrypt bytes
         for t in text.enumerated() {
-            encrypted.append(t.element ^ cipher[t.offset])
+            encrypted.append(t.element ^ cipher[t.offset % key.length])
         }
         return encrypted
     }
@@ -76,17 +75,20 @@ extension StringAnalysisExerciseVC {
         let cipher = [UInt8](key.utf8)
         var decrypted = [UInt8]()
         for t in bytes.enumerated() {
-            decrypted.append(t.element ^ cipher[t.offset])
+            decrypted.append(t.element ^ cipher[t.offset % key.length])
         }
         return String(bytes: decrypted, encoding: .utf8) ?? ""
     }
 
     func retrieveStringTableEntry() -> String {
+        print(SAConstants.stringAnalysisExercise)
         return SAConstants.stringAnalysisExercise
     }
     
     func isValidResponse(proposedText: String) -> Bool {
-        let stringTableEntry = retrieveStringTableEntry()
+        let data = Data(base64Encoded: retrieveStringTableEntry())
+        let buf = [UInt8](data!)
+        let stringTableEntry = decrypt(buf)
         return stringTableEntry.contains(proposedText)
     }
     
@@ -123,6 +125,7 @@ extension String {
 }
 
 */
+
 
 
 
