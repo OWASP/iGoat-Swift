3cadf67d5ce3431b6d439aa526d0fc25102ec583
Augusta Bogie <me@masbog.com>
2018-06-02 05:04:54 +0700

    FIXED : Runtime Brute Force Attack / incorrect PIN (spoil inside ! ;) #4

--- iGoat-Swift/iGoat-Swift/Source/Exercises/Runtime Analysis/BruteForce attack/BruteForceRuntimeVC.swift
+++ iGoat-Swift/iGoat-Swift/Source/Exercises/Runtime Analysis/BruteForce attack/BruteForceRuntimeVC.swift
@@ -23,7 +23,7 @@ class BruteForceRuntimeVC: UIViewController {
         let reference = "0252B081BDA70B478F0131B310A93CB8D79086D785FB4AE392A8C5FFC3DDC5FE"
         let data = ccSha256(data: pin.data(using: .utf8)!)
         let pinSHA256 = data.map { String(format: "%02hhx", $0) }.joined()
-        return reference == pinSHA256
+        return reference == pinSHA256.uppercased()
     }
     
     func ccSha256(data: Data) -> Data {
